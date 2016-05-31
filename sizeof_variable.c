c15c01088ee6f54cb9fa26a41ae32e314cbc57ab
nikhil <nikhil.pawade@Sophos.com>
2016-05-31 17:05:33 +0530

    Sizof variable without sizeof operator.

---
+++ sizeof_variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int *a,*s, v=10;
+	a=&v;
+	s=a;
+	a++;
+	int intsize=(int)a-(int)s;
+	printf("%d\n",intsize);
+	// anothr method 
+	int b;
+	printf("%u\n", (int)(&b+1)-(int)(&b));
+}
