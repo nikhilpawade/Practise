7845af3ad1b3beab7e5afb9d08b767fb948d803a
nikhilpawade <nikhil.nikh.pawade@gmail.com>
2016-07-30 22:40:16 +0530

    Const char * and char const * example

---
+++ example_of_const_char*p_char_const*p.c
@@ -0,0 +1,33 @@
+/*const char * p;
+char * const p; 
+const char * const p;*/ // these are the three conditions,
+
+// const char *p;const char * const p; pointer value cannot be changed
+
+// char * const p; pointer address cannot be changed
+
+// const char * const p; both cannot be changed.
+
+#include<stdio.h>
+
+int main()
+{
+    const char * p; // value cannot be changed
+    char z='a';
+    //*p = 'c'; // this will not work
+    p = &z;
+    printf(" %c\n",*p);
+
+    char * const q; // address cannot be changed
+    char x;
+   *q = 'c'; 
+    //q = &z;   // this will not work
+//    printf(" %c\n",*q);
+
+    const char * const a; // both address and value cannot be changed
+    char b;
+//    *a = 'c'; // this will not work
+  //  a = &b; // this will not work
+    //printf(" %c\n",*a);
+    return 0;
+}
