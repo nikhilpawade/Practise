db5bfa3203c68f570d9bb8310e1753a0d1deee03
nikhilpawade <nikhil.nikh.pawade@gmail.com>
2016-11-15 15:41:33 +0530

    Add reverlinklist

--- LinkList/reverse_linklist.c
+++ LinkList/reverse_linklist.c
@@ -1,8 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct st {
+	int num;
+	struct st *next;
+};
+
+
+insert(struct st **ptr) {
+	struct st *tmp,*cptr,*pptr;
+	tmp = malloc(sizeof(struct st));
+	printf("Enter the number to insert\n");
+	scanf("%d",&(tmp->num));
+	if(*ptr == NULL || (tmp->num < (*ptr)->num)  ) {
+		tmp->next = *ptr;
+		*ptr = tmp;
+	} else {
+		cptr = *ptr;
+		while(cptr->next != NULL && (tmp->num > cptr->num)) {
+			pptr = cptr;
+			cptr = cptr->next;
+		} 
+		if(cptr->num > tmp->num ) {
+			tmp->next = cptr;
+			pptr->next = tmp;	
+		} else {
+			tmp->next = NULL;
+			cptr->next = tmp;
+		}
+
+
+	}
+
+}
+print(struct st *ptr) {
+	while(ptr) {
+		printf("%d ",ptr->num);
+		ptr = ptr->next;
+	}
+
+}
 main()
 {
-	
-
+	struct st *head = NULL;
+	head = malloc(sizeof(struct st));
+	head->num = 10;
+	insert(&head);	
+	print(head);
 
 
 
